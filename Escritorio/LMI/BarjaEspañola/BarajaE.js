da8d7b9d62ccf5237e1b1a8ae9dec61b30a95056
Ivan <igutierrezcontreras@danielcastelao.org>
2025-03-14 10:51:40 +0100

    segundo

---
+++ Escritorio/LMI/BarjaEspañola/BarajaE.js
@@ -0,0 +1,35 @@
+
+let miArray 
+
+
+miArray.push([1,"espadas"])
+miArray.pop()
+miArray.unshift([1,"espadas"])
+miArray.shift()
+
+console.log(miArray)    
+
+const palos = ["espadas", "copas", "bastos", "oros"]
+let barajaEspañola = []
+
+for (let palo of palos){
+    for (let numero = 1; numero <= 10; numero++){
+        barajaEspañola.push([palo, numero])
+    
+        if(numero === 1){
+            barajaEspañola.push(["As",palo])
+        }else if(numero === 8){
+            barajaEspañola.push(["Sota",palo])
+        }else if(numero === 9){
+            barajaEspañola.push(["Caballo",palo])
+
+        }else if(numero === 10){
+            barajaEspañola.push(["Rey",palo])
+        }else {
+            barajaEspañola.push([String.numero,palo])
+        }
+    
+    }
+}
+
+console.log(barajaEspañola)
